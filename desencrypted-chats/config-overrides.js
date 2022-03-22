1f65f846aa7d1881f96892ca0494fa879c4ed7e7
Ahiezer Rada <101113648+ahidroid@users.noreply.github.com>
2022-03-22 09:13:07 -0400

    config-overrides

--- desencrypted-chats/config-overrides.js
+++ desencrypted-chats/config-overrides.js
@@ -1,3 +1,20 @@
 const { useBabelRc, override } = require("customize-cra");
 
 module.exports = override(useBabelRc());
+console.log('override')
+    let loaders = config.resolve
+    loaders.fallback = {
+        "fs": false,
+        "tls": false,
+        "net": false,
+        "http": require.resolve("stream-http"),
+        "https": false,
+        "zlib": require.resolve("browserify-zlib") ,
+        "path": require.resolve("path-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "util": require.resolve("util/"),
+        "crypto": require.resolve("crypto-browserify")
+    }
+    
+    return config
+}
